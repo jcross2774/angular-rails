c2b7a45ed8c50fc1508e2f2d50ee421be25b5b26
James Cross <jcross2774@gmail.com>
2018-02-03 13:14:55 -0700

    Created mock data in angular displayed to user

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,6 +1,7 @@
 
 
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-
+  documents: Document [] = [
+    {
+    title: "My First Doc",
+    description: 'asdfasdf asdf',
+    file_url: 'http://google.com',
+    updated_at: '11/11/16',
+    image_url: 'http://google.com',
+    },
+    {
+      title: "My Second Doc",
+      description: 'asdfasdf asdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Last Doc",
+      description: 'asdfasdf asdf',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com',
+    }
+  ]
 }
